f230d7a1ef1bde968b57a969b717a231f79c7a4f
admin <debian-admin@debian-deploy.debian-deploy.virtualbox.org>
2022-11-28 22:13:35 -0400

    Backend con Docker

---
+++ models/topics.js
@@ -0,0 +1,7 @@
+//models/topics.js
+const mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+var TopicSchema = new Schema({
+    name: String
+});
+module.exports = mongoose.model('Topic', TopicSchema);
